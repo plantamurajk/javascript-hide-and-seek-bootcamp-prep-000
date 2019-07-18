8983ae4a3d236f7181a9789beb5f24caec91b4c9
John Plantamura <plantamurajk@gmail.com>
2019-07-18 20:38:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  var ranks = document.getElementsByClassName('ranked-list')
+  for (let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + 1
+  }
+}
+
+var deepest = document.querySelector('#grand-node').firstElementChild
+
+function deepestChild(node){
+  if (deepest.firstElementChild != null){
+    deepest = deepest.firstElementChild
+    deepest = deepestChild(deepest)
+  }
+  return deepest
+}
